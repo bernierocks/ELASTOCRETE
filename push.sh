5d1c17bb0bedc5ec5d4aea84dce9f52da1b0aca2
Eulr Masauding <logicshaman@gmail.com>
2015-11-16 15:44:22 +0800

    online update

--- push.sh
+++ push.sh
@@ -1,16 +1,3 @@
 git add .
 git commit -m "online update"
 git push
-
-
-
-if [ "$?" != "0" ];
-
-    then
-    # true branch
-    echo "Shell Failed!" 
-else
-    # false branch
-    echo "Shell Success!" 
-    exit 1
-fi
