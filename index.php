56140a1309c672612c3a9a8d65378646f5c91996
Bernie Colita <kittywebworks@gmail.com>
2015-11-16 14:48:25 +0800

    online update

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 		<?php if(have_posts()) : ?>
 		   <?php while(have_posts()) : the_post(); ?>
 			<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-				<?php the_title('<h2>','</h2>'); ?>
+				<?php the_title('<h2 class="bc-page-title">','</h2>'); ?>
 		 		<?php the_content(); ?>
 			</div>
 			<?php
